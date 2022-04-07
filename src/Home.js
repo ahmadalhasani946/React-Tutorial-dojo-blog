6cbab7272ece4a83c2193562abb5bea50785f293
Ahmad Alhasani <ahmad.alhasani@hititcs.com>
2022-04-07 17:07:01 +0300

    lesson 10 map a blogs array and show them on home page

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,20 @@ import { useState } from "react";
 
 const Home = () => {
 
-    // useState return two values the name of the variable and the function we can edit the variable with
-    const [name, setName] = useState("Mario");
-
-    const [age, setAge] = useState(25);
-
-    const handleClick = (e) => {
-        setName("Luigi");
-        setAge(30);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click ME</button>
+            {blogs.map((blog)=> (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written By: { blog.author }</p>
+                </div>
+            ))}
         </div>
      );
 }
