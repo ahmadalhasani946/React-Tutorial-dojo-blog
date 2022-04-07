b87861c6f358b9d4a04c08b500b712ac54b0f8f1
Ahmad Alhasani <ahmad.alhasani@hititcs.com>
2022-04-07 17:48:07 +0300

    lesson 11 props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogsList from "./BlogsList";
 
 const Home = () => {
 
@@ -10,12 +11,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            {blogs.map((blog)=> (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written By: { blog.author }</p>
-                </div>
-            ))}
+            <BlogsList blogs={blogs} title="All Blogs!" />
         </div>
      );
 }
