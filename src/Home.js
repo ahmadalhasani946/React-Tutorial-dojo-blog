7fb7a3db4f8d658372f16e5676faa3c806b6656a
Ahmad Alhasani <ahmad.alhasani@hititcs.com>
2022-04-07 13:08:46 +0300

    lesson 7 Click Handler

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("Hello, ninja" , e);
+    }
+
+    function handleClickAgain(name, e) {
+        console.log(`hello ${name}` , e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click ME</button>
+            <button onClick={(e) => handleClickAgain("Ahmad", e)}>Click ME Again</button>
         </div>
      );
 }
