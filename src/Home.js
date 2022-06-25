5e499461b2805227c02adc88e76cf2ee50086014
Ahmad Alhasani <ahmad.alhasani@hititcs.com>
2022-06-25 20:19:27 +0300

    Lesson 15 useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -9,19 +9,33 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
       ]);
 
+    const [name, setName] = useState("mario");
+
     const deleteHanlder = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id != id);
         setBlogs(newBlogs);
     };
 
+    // since useEffect run with every rendering we want to restrict its running
+    // we add an empty array at the end of the useEffect block so it runs only at the first rendering
+    // we call this array as a dependencies array
+    /*
     useEffect(() => {
         console.log("use Effect ran");
         console.log(blogs);
-    });
+    }, []);
+    */
+
+    useEffect(() => {
+        console.log("use Effect ran");
+        console.log(name);
+    }, [name]);
 
     return ( 
         <div className="home">
             <BlogsList blogs={blogs} title="All Blogs!" deleteHanlder={deleteHanlder} />
+            <button onClick={() => setName("Luigi")}>Change Name</button>
+            <p>{ name }</p>
         </div>
      );
 }
