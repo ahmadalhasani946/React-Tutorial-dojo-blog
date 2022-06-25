a60296208cde5134653d2f27e1d2e7df8437fc9b
Ahmad Alhasani <ahmad.alhasani@hititcs.com>
2022-06-25 23:44:39 +0300

    lesson 19 Handling Fetch Errors

--- src/Home.js
+++ src/Home.js
@@ -4,23 +4,37 @@ import BlogsList from "./BlogsList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true)
+    const [isPending, setIsPending] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [errorCode, setErrorCode] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
-            fetch("http://localhost:8000/blogs")
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-                setIsPending(false);
-            })
+            fetch("http://localhost:8000/blogss")
+                .then((res) => {
+                    if (!res.ok) {
+                        console.log(res)
+                        setErrorCode(res.status);
+                        throw Error("Could not fetch the data for that resource");
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setErrorMessage(null);
+                    setErrorCode(null);
+                })
+                .catch((err) => {
+                    setIsPending(false);
+                    setErrorMessage(err.message);
+                });
         }, 1000)
     }, []);
 
     return ( 
         <div className="home">
+            { errorMessage && <div> { errorCode } : { errorMessage }</div>}
             { isPending && <div>Loading...</div>}
             { blogs && <BlogsList blogs={blogs} title="All Blogs!" />}
         </div>
