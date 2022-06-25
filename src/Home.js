55c1383b07265858e16cc9fe78230660d03ef84d
Ahmad Alhasani <ahmad.alhasani@hititcs.com>
2022-06-25 23:22:24 +0300

    Lesson 18 Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,24 @@ import BlogsList from "./BlogsList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true)
 
     useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-            setBlogs(data);
-        })
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data);
+                setIsPending(false);
+            })
+        }, 1000)
     }, []);
 
     return ( 
         <div className="home">
+            { isPending && <div>Loading...</div>}
             { blogs && <BlogsList blogs={blogs} title="All Blogs!" />}
         </div>
      );
