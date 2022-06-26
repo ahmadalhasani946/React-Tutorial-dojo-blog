477539a624328032ef911dffbcdfa69c8aadca35
Ahmad Alhasani <ahmad.alhasani@hititcs.com>
2022-06-26 15:29:32 +0300

    lesson 25 Route Parameters

--- src/BlogsList.js
+++ src/BlogsList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BlogsList = ({blogs, title}) => {
     return (
         <div className="blogs-list">
             <h1>{title}</h1>
             {blogs.map((blog)=> (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written By: { blog.author }</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{ blog.title }</h2>
+                        <p>Written By: { blog.author }</p>
+                    </Link>
                 </div>
             ))}
         </div>
